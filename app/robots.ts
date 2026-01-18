f8629e398df516b1f2ee7abc066d9880cedcc34a
Seohy <seohy@example.com>
2026-01-18 20:51:26 +0900

    fix: Update robots.txt to explicitly allow Google and AdSense bots

--- app/robots.ts
+++ app/robots.ts
@@ -4,11 +4,21 @@ export default function robots(): MetadataRoute.Robots {
     const baseUrl = 'https://vibecheck.page' // Update this if domain changes
 
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: '/private/',
-        },
+        rules: [
+            {
+                userAgent: 'Googlebot',
+                allow: '/',
+            },
+            {
+                userAgent: 'Mediapartners-Google',
+                allow: '/',
+            },
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: '/private/',
+            },
+        ],
         sitemap: `${baseUrl}/sitemap.xml`,
     }
 }
