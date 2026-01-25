4465acb8e49af93327665c69f001cbf4b23562d4
Seohy <seohy@example.com>
2026-01-25 17:59:02 +0900

    Update banners with Nutri-Match link

--- data/banners.ts
+++ data/banners.ts
@@ -26,5 +26,11 @@ export const banners: Banner[] = [
         imgSrc: "https://irumlab.com/banner_link_234x60.png",
         alt: "이룸랩 배너",
         title: "이룸랩 - 무료 셀프 작명, 영어 닉네임, 브랜드 네이밍"
+    },
+    {
+        href: "https://nutrimatch.kr",
+        imgSrc: "https://nutrimatch.kr/banner_link_234x60.png",
+        alt: "내 몸이 진짜 원하는 영양제는? Nutri-Match 분석 결과 보기",
+        title: "Nutri-Match - 나만의 영양제 궁합 & 저속노화 분석기"
     }
 ];
