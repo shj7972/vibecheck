92b317ee27eb742971b2d8e5b7b0de753c0625a7
Seohy <seohy@example.com>
2026-01-29 21:26:57 +0900

    feat: move AdSense script to head tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,13 +54,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={inter.className}>
-        <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center p-0 sm:p-4 md:p-8 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
-          <main className="w-full max-w-md md:max-w-4xl bg-white min-h-screen sm:min-h-[auto] sm:h-full sm:rounded-3xl shadow-2xl flex flex-col overflow-hidden border-gray-100 sm:border relative">
-            {children}
-          </main>
-        </div>
-        <GoogleAnalytics gaId="G-HLFXV4C2WN" />
+      <head>
         <Script
           id="adsense-init"
           async
@@ -68,6 +62,14 @@ export default function RootLayout({
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+      </head>
+      <body className={inter.className}>
+        <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center p-0 sm:p-4 md:p-8 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
+          <main className="w-full max-w-md md:max-w-4xl bg-white min-h-screen sm:min-h-[auto] sm:h-full sm:rounded-3xl shadow-2xl flex flex-col overflow-hidden border-gray-100 sm:border relative">
+            {children}
+          </main>
+        </div>
+        <GoogleAnalytics gaId="G-HLFXV4C2WN" />
       </body>
     </html>
   );
